8aa882cbb3e326245d7bd8109aadcee15847539d
Peter Jankowski <pjankowski@college.harvard.edu>
2021-11-05 12:18:58 -0400

    successfully imported geojson data and rendered buildings using said data - buildings also hoverable

--- src/App.js
+++ src/App.js
@@ -1,123 +1,95 @@
-
-// // HOOKS BASED IMPLEMENTATION ✅
-
-// import React, {useRef, useState, useEffect} from 'react';
-// import {render} from 'react-dom';
-// import mapboxgl from 'mapbox-gl';
-
-// mapboxgl.accessToken = 'pk.eyJ1IjoicGV0ZXJqYW5rIiwiYSI6ImNrdjl5Zmlzb2E3c2gyd256ZXVicmZ6YWoifQ.uQ0pujB7vKdTIntjIx7q9g'; // Set your mapbox token here
-
-// export default function App() {
-//   const mapContainer = useRef(null);
-//     const map = useRef(null);
-//     const [lng, setLng] = useState(-71.1160);
-//     const [lat, setLat] = useState(42.3752);
-//     const [zoom, setZoom] = useState(16.22);
-
-//   useEffect(() => {
-//     if (map.current) return; // initialize map only once
-//     map.current = new mapboxgl.Map({
-//       container: mapContainer.current,
-//       style: 'mapbox://styles/peterjank/ckvb1vk1u3gap14pcti77y0oo',
-//       center: [lng, lat],
-//       zoom: zoom
-//     });
-//   });
-
-//   return (
-//     <div>
-//       <div ref={mapContainer} className="map-container" 
-//         />
-//     </div>
-//   );
-// }
-
-
-// import React, {useRef, useState, useEffect} from 'react';
-// import {render} from 'react-dom';
-// import mapboxgl from 'mapbox-gl';
-
-// mapboxgl.accessToken = 'pk.eyJ1IjoicGV0ZXJqYW5rIiwiYSI6ImNrdjl5Zmlzb2E3c2gyd256ZXVicmZ6YWoifQ.uQ0pujB7vKdTIntjIx7q9g'; // Set your mapbox token here
-
-// export default function App() {
-//   const [viewport, setViewport] = useState({
-//     latitude: 40,
-//     longitude: -100,
-//     zoom: 3,
-//     bearing: 0,
-//     pitch: 0
-//   });
-
-//   useEffect(() => {
-//     if (map.current) return; // initialize map only once
-//     map.current = new mapboxgl.Map({
-//       container: mapContainer.current,
-//       style: 'mapbox://styles/peterjank/ckvb1vk1u3gap14pcti77y0oo',
-//       center: [lng, lat],
-//       zoom: zoom
-//     });
-//   });
-
-//   return (
-//     <div>
-//       <div ref={mapContainer} className="map-container" 
-//         />
-//     </div>
-//   );
-// }
-
-
-
-// CLASS BASED IMPLEMENTATION ✅
-import React, { Component } from "react";
+import React, { useState, useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoicGV0ZXJqYW5rIiwiYSI6ImNrdjl5Zmlzb2E3c2gyd256ZXVicmZ6YWoifQ.uQ0pujB7vKdTIntjIx7q9g';
+mapboxgl.accessToken = 'pk.eyJ1IjoicGV0ZXJqYW5rIiwiYSI6ImNrdjl5Zmlzb2E3c2gyd256ZXVicmZ6YWoifQ.uQ0pujB7vKdTIntjIx7q9g'; // Set your mapbox token here
 
-class App extends Component {
+export default function App() {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      lng: -71.1160,
-      lat:  42.3752,
-      zoom: 16.22
-    };
-  }
+  const mapContainer = useRef(null);
+    const [lng] = useState(-71.1160);
+    const [lat] = useState(42.3752);
+    const [zoom] = useState(16.22);
+    let hoveredStateId = useState(null);
 
-  componentDidMount() {
-    const { lng, lat, zoom } = this.state;
-
+  useEffect(() => {
     const map = new mapboxgl.Map({
-      container: this.mapContainer,
+      container: mapContainer.current,
       style: 'mapbox://styles/peterjank/ckvb1vk1u3gap14pcti77y0oo',
       center: [lng, lat],
-      zoom
+      zoom: zoom
     });
-		
-     map.on('move', () => {
-      const { lng, lat } = map.getCenter();
 
-      this.setState({
-        lng: lng.toFixed(4),
-        lat: lat.toFixed(4),
-        zoom: map.getZoom().toFixed(2)
+    map.on('load', () => {
+      map.addSource('buildings', {
+        'type': 'geojson',
+        'data': 'https://peter-jankowski.github.io/datavisdemo1/harvardbuildings.geojson',
+        'generateId': true
       });
+
+      map.addLayer({
+        'id': 'building-data',
+        'type': 'line',
+        'source': 'buildings',
+        'layout': {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        'paint': {
+          'line-color': '#ff69b4',
+          'line-width': 3
+        }
+      });
+
+      map.addLayer({
+        'id': 'buildings-fills',
+        'type': 'fill',
+        'source': 'buildings',
+        'layout': {},
+        'paint': {
+          'fill-color': '#627BC1',
+          'fill-opacity': [
+            'case',
+            ['boolean', ['feature-state', 'hover'], false],
+            1,
+            0.5
+          ]
+        }
+      });
+
+      map.on('mousemove', 'buildings-fills', (e) => {
+        if (e.features.length > 0) {
+          if (hoveredStateId !== null) {
+            map.setFeatureState(
+              { source: 'buildings', id: hoveredStateId },
+              { hover: false }
+            );
+          }
+          hoveredStateId = e.features[0].id;
+          map.setFeatureState(
+            { source: 'buildings', id: hoveredStateId },
+            { hover: true }
+          );
+        }
+      });
+
+      map.on('mouseleave', 'buildings-fills', () => {
+        if (hoveredStateId !== null) {
+          map.setFeatureState(
+            { source: 'buildings', id: hoveredStateId },
+            { hover: false }
+          );
+        }
+        hoveredStateId = null;
+      });
+
     });
-  }
+    
+  });
 
-  render() {
-    const { lng, lat, zoom } = this.state;
-
-    return (
-      <div>
-        <div className="inline-block absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
-          <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
-        </div>
-        <div id="map" style={{width: "100vw", height: "100vh"}} ref={el => this.mapContainer = el} className="absolute top right left bottom" />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <div ref={mapContainer} className="map-container" 
+        />
+    </div>
+  );
 }
-
-export default App;
